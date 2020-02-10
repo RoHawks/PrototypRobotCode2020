e37b1603047fffcdc6a8a1834af90f0b2c6ac290
jrmkim50 <jrmkim50@gmail.com>
2020-02-10 17:23:31 -0500

    Made a wrapper for RevSRS

--- src/main/java/common/servos/RevSRS.java
+++ src/main/java/common/servos/RevSRS.java
@@ -4,63 +4,44 @@ import common.servos.configs.interfaces.IPWMConfig;
 import common.servos.interfaces.IAngularServo;
 import common.servos.interfaces.IContinuousServo;
 import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.controller.PIDController;
 
 /**
  * REV Robotics Smart Robot Servo.
  */
-public class RevSRS extends PWM implements IAngularServo, IContinuousServo {
+
+public class RevSRS extends Servo {
     public enum Mode {
         Continuous,
         Angular
     }
 
     private Mode mode;
-    private final double minPeriod = .5d; //milliseconds
-    private final double maxPeriod = 2.5d;
-    private final double centerPeriod = (maxPeriod - minPeriod) / 2d + minPeriod;
-    private final double deadbandMargin = 0.1d;
 
     private final double maxSpeed = 1d;
     private final double minSpeed = -0.998d;
 
-    private final double minAngle = -90d; //is this 130 for our servo?
-    private final double maxAngle = 90d;
+    private double currentSpeed;
 
-    public RevSRS(IPWMConfig config, Mode mode) {
-        super(config.getChannel());
-        this.mode = mode;
-        setBounds(maxPeriod, 
-                  centerPeriod + deadbandMargin, 
-                  centerPeriod, 
-                  centerPeriod - deadbandMargin, 
-                  minPeriod);
+    public RevSRS(int port) {
+        super(port);
+        mode = Mode.Continuous; //If we need a angular mode, then we add another argument to constructor
+        setBounds(2.5, 1.5, 1.5, 1.5, 0.5);
     }
 
     @Override
-    public void setRawAngle(double angle) {
-        if(mode != Mode.Angular) return;
-        //scale angle into 0 to 1 range
-        setPosition((angle - minAngle) / (maxAngle - minAngle));
-    }
-
-    @Override
-    public double getRawAngle() {
-        if(mode != Mode.Angular) return 0;
-        //scale back into angle
-        return minAngle + getPosition() * (maxAngle - minAngle);
-    }
-
-    @Override
-    public void setOutput(double output) {
+    public void setSpeed(double output) {
         if(mode != Mode.Continuous) return;
         if(output < minSpeed) output = minSpeed;
         else if(output > maxSpeed) output = maxSpeed;
+        currentSpeed = output;
         setSpeed(output);
     }
 
     @Override
-    public double getOutput() {
+    public double getSpeed() {
         if(mode != Mode.Continuous) return 0;
-        return getSpeed();
+        return currentSpeed;
     }
 }

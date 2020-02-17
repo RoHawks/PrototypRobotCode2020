a714f2b23d0bc55803dfd4d5db16476b41982db6
Forrest Bicker <bickerforrest@gmail.com>
2020-02-17 17:32:38 -0500

    reformatted RevSRS

--- src/main/java/common/servos/RevSRS.java
+++ src/main/java/common/servos/RevSRS.java
@@ -14,18 +14,16 @@ import edu.wpi.first.wpilibj.controller.PIDController;
  */
 
 public class RevSRS extends Servo {
+
     public enum Mode {
         Continuous,
         Angular
     }
 
     private Mode mode;
-
     private final double maxSpeed = 1d;
     private final double minSpeed = -0.998d;
-
     private double currentSpeed;
-
     private PIDController pid;
 
     public RevSRS(RevSRSConfig config) {
@@ -35,21 +33,33 @@ public class RevSRS extends Servo {
 
         PIDConfig pidConfig = config.getPIDConfig();
 
-        pid = new PIDController(pidConfig.getP(), pidConfig.getI(), pidConfig.getD(), pidConfig.getIZone());
+        pid = new PIDController(
+            pidConfig.getP(),
+            pidConfig.getI(),
+            pidConfig.getD(), 
+            pidConfig.getIZone()
+        );
     }
 
     @Override
     public void setSpeed(double output) {
-        if(mode != Mode.Continuous) return;
-        if(output < minSpeed) output = minSpeed;
-        else if(output > maxSpeed) output = maxSpeed;
+        if (mode != Mode.Continuous) {
+            return;
+        }
+        if (output < minSpeed) {
+            output = minSpeed;
+        } else if (output > maxSpeed) {
+            output = maxSpeed;
+        }
         currentSpeed = output;
         setSpeed(output);
     }
 
     @Override
     public double getSpeed() {
-        if(mode != Mode.Continuous) return 0;
+        if (mode != Mode.Continuous) {
+            return 0;
+        }
         return currentSpeed;
     }
 }
